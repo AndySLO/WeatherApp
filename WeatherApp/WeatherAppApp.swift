79c54c244e375b81911613c6d10322a720db3e2e
Andrey Padalka <andrey.padalka@gmail.com>
2021-09-25 17:04:24 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Andrey Padalka on 9/25/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
